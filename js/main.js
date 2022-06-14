b3710a025d6129462d2009498caa504ee321b1e2
Crisano97 <97keb6@gmail.com>
2022-06-14 16:32:34 +0200

    Prezzo calcolato + percentuale

---
+++ js/main.js
@@ -0,0 +1,36 @@
+//Chiediamo all'utente il numero di km che vuole percorrere;
+//Chiediamo all'utente la sua età;
+//In base ai numeri forniti calcoliamo il prezzo totale del viaggio;
+//il prezzo del biglietto è definito in base ai km(0,20 € al km);
+    // -se l'utente è minorenne va applicato uno sconto del 20%;
+    // - se l'utente è un over 65 va applicato uno sconto del 40%;
+// L'output del prezzo finale andrà mostrato in console con massimo due decimali (toFixed())
+
+const userAge = parseInt ( prompt('Inserisci la tua età') );
+const userKmPath = parseInt ( prompt('Inserisci i km che vuoi percorrere') );
+
+console.log(userAge, userKmPath);
+
+if ( isNaN(userAge) == true ) {
+    console.log('La tua età non è stata accettata, ricarica la pagina e riprova');
+} else if (isNaN(userKmPath) == true ) {
+    console.log('Il numero di km non è stato accettato, ricarica la pagina e riprova');
+} else {
+    let userCost = 0.21 * userKmPath;
+    console.log(userCost);
+
+    let userTotal;
+    
+    
+    if (userAge > 65) {
+        userTotal = (userCost * 40) / 100;
+        console.log(userTotal.toFixed(2))
+
+    } else {
+        userTotal = (userCost * 20) / 100;
+        console.log(userTotal.toFixed(2))
+    }
+
+}
+
+
